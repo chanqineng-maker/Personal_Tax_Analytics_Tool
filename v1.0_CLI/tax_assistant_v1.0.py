51ec13c9b31fcd35b757757d284dde0a285919e5
Chan Qi Neng <chanqineng@gmail.com>
2026-03-18 21:27:40 +0800

    amend

--- v1.0_CLI/tax_assistant_v1.0.py
+++ v1.0_CLI/tax_assistant_v1.0.py
@@ -1,11 +1,11 @@
 annual_income = float(input("Hi! I am your Tax Assistant. What is your annual income? "))
 total_relief = 0
-claimed_reliefs = [] #Keep track of what's been chosen to prevent double claims 
+claimed_reliefs = [] # list to store claimed releifs to prevent double claims 
 
-def claim_spouse_relief(): #Logic for spouse relief
+def claim_spouse_relief(): #function for spouse relief
     income = float(input("\nWhat is your spouse's annual income? "))
-    if income <= 8000:
-        handicapped = input("Is your spouse handicapped? (y/n): ").lower()
+    if income <= 8000:    # income threshold cannot exceed 8k 
+        handicapped = input("Is your spouse handicapped? (y/n): ").lower() # to claim spouse relief for spouses with disability
         if handicapped == "y":
             return 5500
         else:
@@ -17,49 +17,49 @@ def claim_spouse_relief(): #Logic for spouse relief
 
 
 
-def claim_qcr_relief(): #Logic for child relief
+def claim_qcr_relief(): # function for child relief
     try:
-        num_children = int(input("How many children are you claiming for? "))
+        num_children = int(input("How many children are you claiming for? ")) # in case user tries to input non-number values
     except ValueError:
         print("Please enter a valid number.")
         return 0
 
-    total_eligible_children = 0
+    total_elig_children = 0 #counter for total no. of eligible children
     
-    for i in range(1, num_children + 1):
+    for i in range(1, num_children + 1): # loop cycles based on number of children being claimed
         print("\nChecking Child",i)
         
         status = input("Is this child below 16? (y/n): ").lower()
         
         if status == "y":
-            total_eligible_children += 1
+            total_elig_children += 1
         else:
             status1 = input("Is the child studying full-time and unmarried? (y/n): ").lower()
             if status1 == "y":
                 status2 = input("Is the child's annual income $8k or less? (y/n): ").lower()
                 if status2 == "y":
-                    total_eligible_children += 1
+                    total_elig_children += 1 # meets condition
                 else:
-                    print("Child not eligible due to income.")
+                    print("Child not eligible due to their income exceeding the threshold.")
             else:
                 print("Child not eligible due to status.")
-    return total_eligible_children
+    return total_elig_children
 
 
-def claim_wmcr_relief(): #Logic for Working Mother's Child Relief
+def claim_wmcr_relief(): # function for Working Mother's Child Relief
     is_mother = input("Are you a working mother who is married, widowed, or divorced? (y/n): ").lower()
     if is_mother == "y":
-        num_kids = int(input("How many children are you claiming WMCR for? "))
+        num_child = int(input("How many children are you claiming WMCR for? "))
         wmcr_total = 0
-        for i in range(1, num_kids + 1):
+        for i in range(1, num_child + 1):
             born_after_2024 = input("Was Child " + str(i) + " born on or after 1 Jan 2024? (y/n): ").lower()
             if born_after_2024 == "y":
-                # Fixed dollar amount for newer children
+                # Fixed dollar amount calc for children born after 2024
                 if i == 1: wmcr_total += 8000
                 elif i == 2: wmcr_total += 10000
                 else: wmcr_total += 12000
             else:
-                # Percentage based for older children
+                # Percentage based calc for children born before 2024
                 if i == 1: wmcr_total += (annual_income * 0.15)
                 elif i == 2: wmcr_total += (annual_income * 0.20)
                 else: wmcr_total += (annual_income * 0.25)
